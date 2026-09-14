2b4a62695c347f539bdfccbc20dd7502f91017fd
Ahsan Malik <malikahsan7989@gmail.com>
2026-09-14 11:21:11 +0500

    Set www.foldryn.com as canonical URL, fix layout types

--- lib/site.ts
+++ lib/site.ts
@@ -1,3 +1,3 @@
-export const SITE_URL = "https://foldryn.com";
+export const SITE_URL = "https://www.foldryn.com";
 export const SITE_NAME = "Foldryn";
 export const SITE_TAGLINE = "Simple PDF Tools That Work in Your Browser";
